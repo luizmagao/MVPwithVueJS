74c0a1ad490adb144c290afa327b57e746f07fc9
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-26 20:45:56 -0300

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
 
 <template>
+  <nav-bar />
   <div>Here</div>
+  <button class="btn btn-soft btn-neutral">Info</button>
 </template>
