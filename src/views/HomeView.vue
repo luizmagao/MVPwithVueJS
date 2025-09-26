d69a23b4bd90bfedde1f5cccd3c6007f4db4629a
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-26 19:31:00 -0300

    multiplicando components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,15 @@
 <script setup>
-import { ref, watch, watchEffect } from "vue";
 import NavBar from "@/components/NavBar.vue";
-
-const first_name = ref("Luiz");
-
-watch(first_name, (newValue, oldValue) => {
-  console.log(oldValue);
-  console.log(newValue);
-});
-
-const todo = ref(1);
-const data = ref(null);
-
-watchEffect(async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todo.value}`);
-  data.value = await response.json();
-});
+import CardNews from "@/components/CardNews.vue";
 </script>
 
 <template>
   <nav-bar />
 
-  <input type="text" placeholder="neutral" class="input input-neutral" v-model="todo" />
-  <pre>
-    {{ data }}
-  </pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+    <card-news />
+    <card-news />
+    <card-news />
+    <card-news />
+  </div>
 </template>
