8834cd51456b6b63a0728f2e79a5dda55f1a97bf
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-26 20:56:43 -0300

    card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import CardExample from "@/components/CardExample.vue";
 </script>
 
 <template>
   <nav-bar />
-  <div>Here</div>
-  <button class="btn btn-soft btn-neutral">Info</button>
+  <div class="grid grid-cols-1 md:grid-cols-3 grap-3 p-5">
+    <card-example />
+    <card-example />
+    <card-example />
+  </div>
 </template>
