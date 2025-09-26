b16c941028816c5c033565c697600b497250108a
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-26 19:19:51 -0300

    criando components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,9 @@
-<script setup></script>
-
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">Turma p1</a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
-              <li><a>Link 1</a></li>
-              <li><a>Link 2</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div>Here</div>
-  <button class="btn btn-info">Info</button>
+  <nav-bar />
 </template>
+
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, watch, watchEffect } from "vue";
+import NavBar from "@/components/NavBar.vue";
 
 const first_name = ref("Luiz");
 
@@ -18,25 +19,8 @@ watchEffect(async () => {
 </script>
 
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">daisyUI</a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
-              <li><a>Link 1</a></li>
-              <li><a>Link 2</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <nav-bar />
+
   <input type="text" placeholder="neutral" class="input input-neutral" v-model="todo" />
   <pre>
     {{ data }}
